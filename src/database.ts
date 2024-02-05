fa3664327d3093f03423327458ddf483aba8c567
Davor Stefanovski <davorflute@gmail.com>
2024-02-05 21:53:15 +0100

    try

---
+++ src/database.ts
@@ -0,0 +1,63 @@
+import { DefaultValueAccessor } from "@angular/forms";
+
+export const LIST: any = [
+    {
+        id:0,
+        name:"Small apartment in Kalithea",
+        type:"apartment",
+        area:36,
+        price:92000,
+        picture:"https://i.pinimg.com/736x/ca/9d/1f/ca9d1f1c45a7bc56fd75021827fb0e1a.jpg",
+        description:"This apartment is beachfront."
+
+
+    },
+    {
+        id:1,
+        name:"Medium apartment in Perea",
+        type:"apartment",
+        area:61,
+        price:154000,
+        picture:"https://www.bhg.com/thmb/Ke48CUYRrXfAc_CJP4eXuZsrWkw=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/living-room-gallery-shelves-l-shaped-couch-ELeyNpyyqpZ8hosOG3EG1X-b5a39646574544e8a75f2961332cd89a.jpg",
+        description:"Very quiet town."
+    },
+    {
+        id:2,
+        name:"Big house",
+        type:"house",
+        area:220,
+        price:300000,
+        picture:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Atat%C3%BCrk_Museum_%28Thessaloniki%29_by_Joy_of_Museums.jpg/1024px-Atat%C3%BCrk_Museum_%28Thessaloniki%29_by_Joy_of_Museums.jpg",
+        description:"It has a big balcony."
+    },
+    {
+        id:3,
+        name:"Apartment in center of city",
+        type:"apartment",
+        area:71,
+        price:150000,
+        picture:"https://assets-global.website-files.com/6298896e3cd8b96e5bded05d/63cae0a8c74a322f3bcd2afb_frisco-new-apartments-dallas-thumbnail.jpg",
+        description:"Amazing kitchen"
+    }
+
+];
+
+export const LIST1: any = [
+    {
+        id:0,
+        username:"Davorstefanovski",
+        password:"pasvord"
+    },
+    {
+        id:1,
+        username:"angela",
+        password:"pasvord"
+    }
+];
+
+export const LIST2: any =[ 
+    {
+        idUser:0,
+        idProperties:[]
+    }
+];
